2dc0aa653b93ca2adcef2d06e2e5939fb193f4a5
Ulrich Soeffing <ulrich_soeffing@gmx.de>
2013-07-04 09:52:31 +0000

    read index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var content = fs.readFileSync('index.html');
-  response.send(content.toString);
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
